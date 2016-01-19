b23b1251c5e74c34dfde510b953270d863e3e5eb
Fabio Picheli <fabio_picheli@Captain-America.local>
2016-01-19 14:17:11 -0200

    builded fix

--- dist/angular-json-schema.js
+++ dist/angular-json-schema.js
@@ -1,155 +1,157 @@
-/*
+;/*
  * Angular Rest Route
  * Work with Angular UI Router (https://github.com/angular-ui/ui-router)
  * Created by: Fábio Picheli (github.com/picheli20)
  */
-;(function () {
-  'use strict'
+(function() {
+  'use strict';
 
-  function ngSchemaProvider () {
-    var em
-    var vm = this
-    var attrs = {}
+  function ngSchemaProvider() {
+    var em;
+    var vm = this;
+    var attrs = {};
 
-    function getTemplate (ngEntityManager, $stateParams) {
-      return ngEntityManager.getLayout(attrs, $stateParams)
+    function getTemplate(ngEntityManager, $stateParams) {
+      return ngEntityManager.getLayout(attrs, $stateParams);
     }
 
-    function validateId (id) {
-      return !id
-    }
-
-    /**
-    * Instanciate a object and put all the methods
-    *
-    * @method extend
-    * @param {Object} buildedClass A instanced object.
-    * @param {id} id The ng-schema-id value of the component
-    * @return {Object} Return a extended obejct
-    */
-
-    function extend (buildedClass, id) {
-      if (validateId(id)) {
-        return {}
-      }
-      return angular.extend(buildedClass, em.getClass(id))
+    function validateId(id) {
+      return !id;
     }
 
     /**
-    * Instanciate a object and put all the methods
-    *
-    * @method instance
-    * @param {Object} object A object to be instanced
-    * @param {id} id The ng-schema-id value of the component
-    * @return {Object} Return a instanced class with all the methods
-    */
-    function instance (Object, id) {
+     * Instanciate a object and put all the methods
+     *
+     * @method extend
+     * @param {Object} buildedClass A instanced object.
+     * @param {id} id The ng-schema-id value of the component
+     * @return {Object} Return a extended obejct
+     */
+
+    function extend(buildedClass, id) {
       if (validateId(id)) {
-        return {}
+        return {};
       }
-      var instance = {}
-      return extend(instance, id)
+      return angular.extend(buildedClass, em.getClass(id));
     }
 
-    function set (newAttrs) {
-      attrs = newAttrs
-      attrs.templateProvider = ['ngEntityManager', '$stateParams', getTemplate]
-      return attrs
+    /**
+     * Instanciate a object and put all the methods
+     *
+     * @method instance
+     * @param {Object} object A object to be instanced
+     * @param {id} id The ng-schema-id value of the component
+     * @return {Object} Return a instanced class with all the methods
+     */
+    function instance(Object, id) {
+      if (validateId(id)) {
+        return {};
+      }
+      var instance = {};
+      return extend(instance, id);
     }
 
-    vm.$get = function (ngEntityManager) {
-      em = ngEntityManager
+    function set(newAttrs) {
+      attrs = newAttrs;
+      attrs.templateProvider = ['ngEntityManager', '$stateParams', getTemplate];
+      return attrs;
+    }
+
+    vm.$get = function(ngEntityManager) {
+      em = ngEntityManager;
       return {
         'extend': extend,
         'instance': instance
-      }
-    }
-    vm.set = set
+      };
+    };
+    vm.set = set;
   }
   /**
-  * Main provider.
-  *
-  * @class ngSchema
-  */
+   * Main provider.
+   *
+   * @class ngSchema
+   */
   angular
     .module('angular-json-schema', ['ui.router'])
-    .provider('ngSchema', [ngSchemaProvider])
-})()
+    .provider('ngSchema', [ngSchemaProvider]);
+})();
 
 ;(function () {
-  'use strict'
+  'use strict';
   function ngComponentFactory () {
-    return function () {}
+    return function () {};
   }
 
   angular
     .module('angular-json-schema')
-    .factory('ngComponent', [ngComponentFactory])
-})()
+    .factory('ngComponent', [ngComponentFactory]);
+})();
 
 ;(function () {
-  'use strict'
+  'use strict';
 
   function ngEntityManagerFactory ($http, ngUtil, ngEntityObject) {
-    var attrs = {}
+    var attrs = {};
     // TO-DO store the objects to make the lib faster
-    var storedAttrs = {}
+    var storedAttrs = {};
 
     function getInvalidMessage (attr, response) {
-      return 'Please check the json response of (<b>' + response.config.method + '</b>) <b>' + response.config.url + '</b> has no <b>' + attr + '</b> attribute. <a href="http://json-schema.org/"" target="_blank">More info</a>'
+      return 'Please check the json response of (<b>' + response.config.method + '</b>) <b>' + response.config.url + '</b> has no <b>' + attr + '</b> attribute. <a href="http://json-schema.org/"" target="_blank">More info</a>';
     }
 
     function buildUrl (params) {
-      var url = '/schema' + attrs.url
+      var url = '/schema' + attrs.url;
       angular.forEach(params, function (value, key) {
-        url = url.replace(':' + key, value)
-      })
+        url = url.replace(':' + key, value);
+      });
       // return url
-      return '/data/pageSchema.json'
+      return '/data/pageSchema.json';
     }
 
     function entitysCreation (data) {
       // Create the page Object
-      var po = storedAttrs[data.id] = data = new ngEntityObject(data)
+      var po = storedAttrs[data.id] = data = new ngEntityObject(data);
 
       // Create and redering the templates
-      var template = ''
-      var props = data.properties
+      var template = '';
+      var props = data.properties;
       if (props && !props.length) {
-        props = ngUtil.bubbleSort(props, 'propertyOrder')
+        props = ngUtil.bubbleSort(props, 'propertyOrder');
       }
 
       angular.forEach(props, function (value, key) {
-        var id = ngUtil.random()
-        template += '<' + value.type + ' ng-schema-id="' + id + '">'
-        value['parent'] = po
+        var id = ngUtil.random();
+        template += '<' + value.type + ' ng-schema-id="\'' + id + '\'">';
+        value['parent'] = po;
         if (!value['id']) {
-          value['id'] = id
+          value['id'] = id;
         }
-        template += entitysCreation(value)
-        template += '</' + value.type + '>'
+        template += entitysCreation(value);
+        template += '</' + value.type + '>';
 
-      })
+      });
 
-      return template
+      return template;
     }
 
     return {
       'getLayout': function (attrsNew, $stateParams) {
-        attrs = attrsNew
+        attrs = attrsNew;
         return $http.get(buildUrl($stateParams), {'headers': {'Accept': 'application/json'}}).then(function (response) {
-          if (!response.data.properties) return getInvalidMessage('properties', response)
-          return entitysCreation(response.data)
+          if (!response.data.properties){
+            return getInvalidMessage('properties', response);
+          }
+          return entitysCreation(response.data);
         }, function () {
-          console.error('error')
-        })
+          console.error('error');
+        });
       },
       'getClass': function (id) {
-        return storedAttrs[id]
+        return storedAttrs[id];
       },
       'entitysCreation': entitysCreation,
       'storedAttrs': storedAttrs
-    }
+    };
   }
   /**
   * Manage all the entitys for the template creation, etc...
@@ -159,76 +161,89 @@
 
   angular
     .module('angular-json-schema')
-    .factory('ngEntityManager', ['$http', 'ngUtil', 'ngEntityObject', ngEntityManagerFactory])
-})()
+    .factory('ngEntityManager', ['$http', 'ngUtil', 'ngEntityObject', ngEntityManagerFactory]);
+})();
 
 ;(function () {
-  'use strict'
+  'use strict';
 
   function ngEntityObjectFactory (ngUtil) {
     return function (attrs) {
-      var vm = this
-      angular.extend(vm, attrs)
+      var vm = this;
+      angular.extend(vm, attrs);
 
-      if (vm.links)
+      if (vm.links){
         for (var i = 0; i < vm.links.length; i++) {
           // ngEntityObject.entitysCreation(link)
-          create(vm.links[i])
+          create(vm.links[i]);
+        }
       }
 
       // Create the abstract methods for the links actions
       function create (link) {
         if (!link.rel) {
-          return
+          return;
         }
         vm[link.rel] = function (object) {
           // callback, beforeAction
-          if (object.beforeAction) object.beforeAction()
+          if (object.beforeAction){
+            object.beforeAction();
+          }
 
-          var requiredError = false
+          var requiredError = false;
 
           if (link.schema && link.schema.required) {
             // TO-DO check all the field if its Ok
             for (var i = 0; i < link.schema.required.length; i++) {
-              var label = link.schema.required[i]
+              var label = link.schema.required[i];
               if (!vm[label]) {
-                console.error('The ' + label + ' attribute is required.')
-                if (object.validationError) object.validationError(label)
-                requiredError = true
+                console.error('The ' + label + ' attribute is required.');
+                if (object.validationError){
+                  object.validationError(label);
+                }
+                requiredError = true;
               // return
               }
             }
-            console.log('Required infos', link.schema.required)
+            console.log('Required infos', link.schema.required);
           }
 
-          if (!requiredError) makeRequest(link)
+          if (!requiredError){
+            makeRequest(link);
+          }
 
-          if (object.callback) object.callback()
-        }
+          if (object.callback){
+            object.callback();
+          }
+        };
       }
 
       function makeRequest (link) {
-        if (!link.href) return
-        var requestURL = link.href
-
-        if (!link.method) link.method = 'GET'
-        var params = ngUtil.parseURL(link.href)
-        for (var i = 0; i < params.length; i++) {
-          var param = params[i]
-          requestURL = requestURL.replace(param[0], vm[param[1]])
+        if (!link.href){
+          return;
         }
-        console.log('final url', requestURL)
+        var requestURL = link.href;
+
+        if (!link.method){
+          link.method = 'GET';
+        }
+        var params = ngUtil.parseURL(link.href);
+        for (var i = 0; i < params.length; i++) {
+          var param = params[i];
+          requestURL = requestURL.replace(param[0], vm[param[1]]);
+        }
+        console.log('final url', requestURL);
       }
-    }
+    };
   }
 
   angular
     .module('angular-json-schema')
-    .factory('ngEntityObject', ['ngUtil', ngEntityObjectFactory])
-})()
+    .factory('ngEntityObject', ['ngUtil', ngEntityObjectFactory]);
+})();
 
 ;(function () {
-  'use strict'
+  'use strict';
 
   angular
     .module('angular-json-schema')
@@ -237,40 +252,44 @@
   function ngUtilFactory () {
     return {
       'bubbleSort': function (object, property) {
-        var array = []
+        var array = [];
         angular.forEach(object, function (value, key) {
-          this.push(value)
-        }, array)
+          this.push(value);
+        }, array);
 
         // Bubble sort
-        var aux
+        var aux;
         for (var i = array.length - 1; i >= 1; i--) {
           for (var y = 0; y < i; y++) {
-            if (array[y][property] && isNaN(array[y][property])) array[y][property] = Infinity
-            if (array[y + 1][property] && isNaN(array[y + 1][property])) array[y + 1][property] = Infinity
+            if (array[y][property] && isNaN(array[y][property])){
+              array[y][property] = Infinity;
+            }
+            if (array[y + 1][property] && isNaN(array[y + 1][property])){
+              array[y + 1][property] = Infinity;
+            }
             if (array[y][property] > array[y + 1][property]) {
-              aux = array[y]
-              array[y] = array[y + 1]
-              array[y + 1] = aux
+              aux = array[y];
+              array[y] = array[y + 1];
+              array[y + 1] = aux;
             }
           }
         }
-        return array
+        return array;
       },
       'random': function () {
-        return btoa(Math.random())
+        return btoa(Math.random());
       },
       'parseURL': function (url) {
-        var matchs = []
-        var re = /{([\s\S]*?)}/gm
-        var match
+        var matchs = [];
+        var re = /{([\s\S]*?)}/gm;
+        var match;
 
-        while (match = re.exec(url)) {
-          matchs.push(match)
+        while (match = re.exec(url)){
+          matchs.push(match);
         }
 
-        return matchs
+        return matchs;
       }
-    }
+    };
   }
-})()
+})();
